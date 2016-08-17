63cdb07539f9665b9834200b1f430580f97347a5
Frank Hyunsok Oh <enshahar@gmail.com>
2016-08-17 23:18:51 +1000

    - changed FP turtle to enable |> pipe operator. - added FP turtle client - refactoring AWTGrphics and StdoutLogger

--- src/main/scala/com/enshahar/turtle/common/Graphics.scala
+++ src/main/scala/com/enshahar/turtle/common/Graphics.scala
@@ -1,5 +1,9 @@
 package com.enshahar.turtle.common
 
+import java.awt.Graphics2D
+
+import scala.swing.Panel
+
 /**
   * Created by hyunsok on 2016-08-14.
   */
@@ -11,4 +15,25 @@ trait Graphics {
   def moveTo(pos: Position)
   def setColor(color: Color)
   def clear()
+}
+
+trait AWTGraphics extends Graphics {
+  val gr: Graphics2D
+  val canvas: Panel
+  val WIDTH : Int
+  val HEIGHT: Int
+
+  // graphics methods
+  var prevPos: Position = (0.0, 0.0)
+  override def lineTo(pos: Position) =  {
+    gr.drawLine(prevPos._1.toInt, prevPos._2.toInt, pos._1.toInt, pos._2.toInt)
+    prevPos = pos
+  }
+  override def moveTo(pos: Position) =  { prevPos = pos; }
+  override def setColor(color: Color) = { gr.setColor(new java.awt.Color(color._1, color._2, color._3)) }
+  override def clear() = {
+    gr.setColor( java.awt.Color.BLACK )
+    gr.drawRect( -WIDTH / 2,-HEIGHT / 2, WIDTH, HEIGHT )
+    canvas.repaint()
+  }
 }
